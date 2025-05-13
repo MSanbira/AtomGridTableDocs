e96ded280bc833f64f1074669589d46010d3b3db
‪Matan Sanbira‬‏ <myt.comp@gmail.com>
2025-05-13 23:20:09 +0200

    publish work

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { AtomGridTable } from '@sanbira/atom-grid-table';
-import '@sanbira/atom-grid-table/style.css';
-
 function App() {
   return (
     <div>
-      <AtomGridTable
-        colOptions={[
-          { name: "id", label: "ID", width: "70px" },
-          { name: "name", label: "Name", width: "130px" },
-        ]}
-        rows={[
-          { cells: [{ content: "1" }, { content: "Alice" }] },
-          { cells: [{ content: "2" }, { content: "Bobie" }] },
-        ]}
-      />
+      hello world
     </div>
   );
 }
